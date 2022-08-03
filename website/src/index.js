071f78686ea0e73832d347f22a9fb1b60de28499
Kloodi2021 <95110627+Kloodi2021@users.noreply.github.com>
2022-08-03 23:18:35 +0300

    Delete website directory

--- website/src/index.js
+++
@@ -1,8 +0,0 @@
-function GetParameter(parameterName) {
-    let params = new URLSearchParams(window.location.search);
-    return params.get(parameterName);
-}
-
-function ChangeText(id, text) {
-    document.getElementById(id).innerHTML = text;
-}
